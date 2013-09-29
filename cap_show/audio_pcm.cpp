d234532991a67443a30841fc284413f190dab443
yanzhiqiang <f_1s2t3f4f5@126.com>
2013-09-29 18:32:03 +0800

    add faac

--- cap_show/audio_pcm.cpp
+++ cap_show/audio_pcm.cpp
@@ -3,7 +3,7 @@
 #include <alsa/asoundlib.h>
 
 
-int l_audio_open(snd_pcm_stream_t play_mode,char* device_name,int sample_rate,int channels,snd_pcm_format_t cap_bit)
+snd_pcm_t* l_audio_open(snd_pcm_stream_t play_mode,char* device_name,int sample_rate,int channels,snd_pcm_format_t cap_bit)
 {
 	snd_pcm_t *handle=NULL;
 	snd_pcm_hw_params_t *hw_params=NULL;
@@ -11,49 +11,49 @@ int l_audio_open(snd_pcm_stream_t play_mode,char* device_name,int sample_rate,in
 	if ((err = snd_pcm_open (&handle, device_name, play_mode, 0)) < 0)
 	{
 		log_to_file(FLOG_ERR,"cannot open audio device %s (%s)/n", argv[1],snd_strerror (err));
-		return -1;
+		return NULL;
 	}
 
 	if ((err = snd_pcm_hw_params_malloc (&hw_params)) < 0) 
 	{
 		log_to_file(FLOG_ERR,"cannot allocate hardware parameter structure (%s)/n",snd_strerror (err));
-		return -1;
+		return NULL;
 	}
 
 	if ((err = snd_pcm_hw_params_any (handle, hw_params)) < 0) 
 	{
 		log_to_file(FLOG_ERR,"cannot initialize hardware parameter structure (%s)/n",snd_strerror (err));
-		return -1;
+		return NULL;
 	}
 
 	if ((err = snd_pcm_hw_params_set_access (handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) 
 	{
 		log_to_file(FLOG_ERR,"cannot set access type (%s)/n",snd_strerror (err));
-		return -1;
+		return NULL;
 	}
 
 	if ((err = snd_pcm_hw_params_set_format (handle, hw_params, cap_bit)) < 0) 
 	{
 		log_to_file(FLOG_ERR,"cannot set sample format (%s)/n",snd_strerror (err));
-		return -1;
+		return NULL;
 	}
 
 	if ((err = snd_pcm_hw_params_set_rate_near (handle, hw_params, &sample_rate, 0)) < 0) 
 	{
 		log_to_file(FLOG_ERR,"cannot set sample rate (%s)/n",snd_strerror (err));
-		return -1;
+		return NULL;
 	}
 
 	if ((err = snd_pcm_hw_params_set_channels (handle, hw_params, channels)) < 0) 
 	{
 		log_to_file(FLOG_ERR,"cannot set channel count (%s)/n",snd_strerror (err));
-		return -1;
+		return NULL;
 	}
 
 	if ((err = snd_pcm_hw_params (handle, hw_params)) < 0) 
 	{
 		log_to_file(FLOG_ERR,"cannot set parameters (%s)/n",snd_strerror (err));
-		return -1;
+		return NULL;
 	}
 
 	snd_pcm_hw_params_free (hw_params);
@@ -61,6 +61,26 @@ int l_audio_open(snd_pcm_stream_t play_mode,char* device_name,int sample_rate,in
 	if ((err = snd_pcm_prepare (handle)) < 0) 
 	{
 		log_to_file(FLOG_ERR,"cannot prepare audio interface for use (%s)/n",snd_strerror (err));
+		return NULL;
+	}
+}
+
+int l_audio_capture(snd_pcm_t* capture_handle,char* context,const int frame_num)
+{
+	int err=0;
+	if ((err = snd_pcm_readi (capture_handle, context, frame_num)) != frame_num) 
+	{
+		log_to_file(FLOG_ERR,"read audio interface failed (%s)/n",snd_strerror (err));
+		return -1;
+	}
+}
+
+int l_audio_playback(snd_pcm_t* playback_handle,char* context,const int frame_num)
+{
+	int err=0;
+	if ((err = snd_pcm_writei (playback_handle, context, frame_num)) != frame_num) 
+	{
+		log_to_file(FLOG_ERR,"write to audio interface failed (%s)/n",snd_strerror (err));
 		return -1;
 	}
 }
--- cap_show/audio_pcm.h
+++ cap_show/audio_pcm.h
@@ -2,16 +2,16 @@
 #define __AUDIO_PCM_H
 
 #define LINUX_AUDIO_PLAYBACK	0
-#define LINUX_AUDIO_CAPTURE		1
+#define LINUX_AUDIO_CAPTURE	1
 
 //linux open audio device for capture or playback
 //play_mode : SND_PCM_STREAM_PLAYBACK or SND_PCM_STREAM_CAPTURE
-int l_audio_open(snd_pcm_stream_t play_mode,char* device_name,unsigned int sample_rate=44100,int channels=2,snd_pcm_format_t cap_bit=SND_PCM_FORMAT_S16_LE);
+snd_pcm_t* l_audio_open(snd_pcm_stream_t play_mode,char* device_name,unsigned int sample_rate=44100,int channels=2,snd_pcm_format_t cap_bit=SND_PCM_FORMAT_S16_LE);
 
 //linux capture audio
-int l_audio_capture(char* context,const int length);
+int l_audio_capture(snd_pcm_t* capture_handle,char* context,const int frame_num);
 
 //linux playback audio
-int l_audio_playback(char* context,const int length);
+int l_audio_playback(snd_pcm_t* playback_handle,char* context,const int frame_num);
 
 #endif
